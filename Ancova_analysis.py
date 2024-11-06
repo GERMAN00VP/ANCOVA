d693b6c8e8088ce494c451b07909f19f1079a9a1
GERMAN00VP <vallejopalma.g@gmail.com>
2024-11-06 09:31:47 +0100

    Post-Hoc tests

--- Ancova_analysis.py
+++ Ancova_analysis.py
@@ -9,12 +9,14 @@ from statsmodels.formula.api import ols
 from scipy.stats import levene
 from scipy.stats import shapiro
 from itertools import combinations
-
+import statsmodels as sm
 import matplotlib.pyplot as plt
 import seaborn as sns
 from statsmodels.stats.anova import anova_lm
 import matplotlib.pyplot as plt
 import seaborn as sns
+import scikit_posthocs as sp
+
 
 
 
@@ -80,8 +82,87 @@ def interactions_translator(interactions,original_columns,translated_columns):
     return new_interactions
 
 
+
+def convert_dun(dunn_results):
+    group1=dunn_results.columns[0],dunn_results.columns[0],dunn_results.columns[1]
+    group2=dunn_results.columns[1],dunn_results.columns[2],dunn_results.columns[2]
+    padj = dunn_results.iloc[0,1],dunn_results.iloc[0,2],dunn_results.iloc[1,2]
+    return pd.DataFrame([group1,group2,padj],index=["group1",'group2',"p-adj"]).T
+
+
+
+
+def add_significance(ax, y_base, height, posthoc, order, fixed_vertical_height=0.5):
+    """
+    Adds significance brackets with asterisks to indicate statistical significance between pairs of categories on a Seaborn plot.
+
+    Parameters:
+    -----------
+    ax : matplotlib.axes.Axes
+        The axis object where the brackets and asterisks will be added.
+    y_base : float
+        The base height on the y-axis where the first significance bracket will be drawn.
+    height : float
+        The vertical spacing between each significance bracket.
+    Post-hoc : pandas.DataFrame
+        The DataFrame containing the results of the post-hoc test.
+    order : list of str
+        The custom order for the categories on the x-axis.
+    fixed_vertical_height : float, default=0.5
+        The fixed height of the vertical lines in the brackets.
+
+    Returns:
+    --------
+    None
+    """
+    # Map each category label to its specified position on the x-axis
+    x_positions = {label: pos for pos, label in enumerate(order)}
+
+    significance_data = list(zip(zip(posthoc.group1,posthoc.group2),posthoc['p-adj']	))
+
+    # Iterate over each pair of groups in significance_data
+    for i, ((group1, group2), p_val) in enumerate(significance_data):
+        if p_val < 0.05:  # Only add brackets for significant comparisons
+            # Get x positions for the specified groups
+            x1, x2 = x_positions[group1], x_positions[group2]
+            y = y_base + i * height  # Set the y position for the current bracket
+
+            # Draw the significance bracket
+            ax.plot([x1, x1, x2, x2], 
+                    [y, y + fixed_vertical_height, y + fixed_vertical_height, y], 
+                    color='black')
+
+            # Determine the asterisks based on the p-value
+            if p_val < 0.001:
+                stars = '***'
+            elif p_val < 0.01:
+                stars = '**'
+            elif p_val < 0.05:
+                stars = '*'
+            else:
+                stars = ''  # Optional: '' for non-significant
+
+            # Place the asterisks above the bracket
+            ax.text((x1 + x2) * 0.5, y + fixed_vertical_height, stars, 
+                    ha='center', va='bottom', color='black', fontsize=13, fontweight=900)
+
+
+def eliminate_cat_variable(formula):
+
+    terms= formula.split("~")
+
+    terms[1] = "+".join(terms[1].split("+")[1:])
+
+    new_formula = "~".join(terms)
+
+    return new_formula
+
+
+
+
 def do_ancova(data:pd.DataFrame,interactions:list|str=None,
-              plot:bool=False, save_plot:bool|str=False,covariate_to_plot:str=None,palette:dict=None ):
+              plot:bool=False, save_plot:bool|str=False,covariate_to_plot:str=None,palette:dict=None,
+              y_lab=False,x_lab=False ):
     
     """ Function that allows you to make parametrical or non-parametrical ancovas.
 
@@ -160,35 +241,73 @@ def do_ancova(data:pd.DataFrame,interactions:list|str=None,
 
     shapiro_test = shapiro(residuals).pvalue>0.05
     levene_test = levene_test.pvalue > 0.05
+    ph = None
 
     if shapiro_test and levene_test:
         # Fit normal anova to the model
         anova_results = anova_lm(model, typ=2)
+        
+
+        if anova_results.loc[f"C({data.columns[1]})"]["PR(>F)"]<0.05 and len(residual_groups)==3:
+
+            # Adjust the model for the covariables
+            model_covariables = ols(eliminate_cat_variable(formula), data=data).fit()
+            
+            # Extract the data adjusted for the covariables
+            data["Adj_data"]= model_covariables.predict().mean()+model_covariables.resid
+            
+            # Post-hoc test Tukey
+            ph =  pd.DataFrame(sm.stats.multicomp.pairwise_tukeyhsd(endog=data["Adj_data"], 
+                                                                       groups=data[data.columns[1]], alpha=0.05).summary().data)
+            ph.columns=ph.loc[0]
+            ph.drop(0,inplace=True)
+            
+
 
     else:
-        print("Data not normal or not homoscedastic, doing the analyisis in the ranked data:")
         # Convert the response to ranked response
         data[data.columns[0]] = data[data.columns[0]].rank()
         model = ols(formula, data=data).fit()
         anova_results = anova_lm(model, typ=2)
 
-        
-    results_dict = {"Target":target, "Categorical condition":categorical_var, "Co-variables":covars, "Res-Normality":shapiro_test,
-                    "Res-Homoscedasticity":levene_test,"Formula":pretty_formula, "N":N}
-    
-    # Do a combined plot with the results
-    if plot:
+        if anova_results.loc[f"C({data.columns[1]})"]["PR(>F)"]<0.05 and len(residual_groups)==3:
 
+            # Adjust the model for the covariables
+            model_covariables = ols(eliminate_cat_variable(formula), data=data).fit()
+            # Extract the data adjusted for the covariables
+            adjusted = model_covariables.predict().mean()+model_covariables.resid
+            predicted_groups = [adjusted[data[data.columns[1]]==level] for level in data[data.columns[1]].unique()]
+            # Post-hoc test Dunn test on the residuals
+            dunn_results = sp.posthoc_dunn(predicted_groups, p_adjust='holm')
+            dunn_results.columns=data[data.columns[1]].unique()
+            dunn_results.index=data[data.columns[1]].unique()
+            ph = convert_dun(dunn_results)
+
+
+
+
+    results_dict = {"Target":target, "Categorical condition":categorical_var, "Co-variables":covars, "Res-Normality":shapiro_test,
+                    "Res-Homoscedasticity":levene_test,"Formula":pretty_formula, "N":N,
+                    "P.val (Categorical condition)":round(anova_results.loc[f"C({categorical_var})"]["PR(>F)"],4),
+                    "P.val (Co-variable)":round(anova_results.loc[f"{data.columns[2]}"]["PR(>F)"],4)}
+
+
+#########################
+## PLOT
+#########################
+    
+    if plot:    
+        
         # Define the optional values if not provided
         if palette is None:
             palette = generar_diccionario_colores(data[data.columns[1]].unique().tolist())
-                                                  
+                                                    
         if covariate_to_plot is None:
             covariate_to_plot=data.columns[2]
 
 
         # Crear subplots
-        fig, axs = plt.subplots(1, 2, figsize=(12, 6))
+        fig, axs = plt.subplots(1, 2, figsize=(12, 5))
 
         # Añadir líneas de regresión para cada grupo en hue
         for group in data_pl[categorical_var].unique():
@@ -236,14 +355,22 @@ def do_ancova(data:pd.DataFrame,interactions:list|str=None,
         else: 
             # ADD SIGNIFICANCE AS TITLE
             axs[0].set_title(f"P-value: {pval_covariable}")
+            
 
         axs[0].legend()
 
-        # The cathegorical plot
+        ### The cathegorical plot
 
         sns.boxplot(data=data_pl,y=target,x=categorical_var,
                     palette=palette,ax=axs[1])
-        
+
+        # If the post hoc was performed, add the brackets and asterisks
+        if not ph is None:
+            ylim = axs[1].get_ylim()
+            # Agregar los corchetes de significancia
+            add_significance(axs[1], y_base=ylim[1], height=ylim[1]/18,posthoc=ph, 
+                            order=data[data.columns[1]].unique(), fixed_vertical_height=ylim[1]/50)
+                
 
         pval_categorical = anova_results.loc[f"C({categorical_var})"]["PR(>F)"]
 
@@ -255,7 +382,16 @@ def do_ancova(data:pd.DataFrame,interactions:list|str=None,
         # ADD SIGNIFICANCE AS TITLE
         axs[1].set_title(f"P-value: {pval_categorical}")
 
-    
+
+        # Set the axis labels if provided:
+        if y_lab:
+            axs[0].set_ylabel(y_lab)
+            axs[1].set_ylabel(y_lab)
+        if x_lab:
+            axs[0].set_xlabel(x_lab)
+            axs[1].set_xlabel("")
+        
+
         # Guardar la figura
         if save_plot:
             plt.savefig(save_plot,bbox_to_inches="tight")
@@ -263,5 +399,5 @@ def do_ancova(data:pd.DataFrame,interactions:list|str=None,
         # Mostrar la figura
         plt.show()
 
-
+        
     return pd.DataFrame(results_dict),anova_results
